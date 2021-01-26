20837278ae35ef386ba2ae3c23ab71e5755daebb
Aster <192608617@qq.com>
2021-01-26 22:28:04 +0800

    🔧 Add test cases for wolfram importer

---
+++ projects/wolfram-bind/PacletInfo.m
@@ -0,0 +1,10 @@
+Paclet[
+    Name -> "ImportX",
+    Version -> "0.1",
+    MathematicaVersion -> "12.1",
+    Description -> "Extends Wolfram Import Function",
+    Creator -> "Aster 192608617@qq.com",
+    Extensions -> {
+        {"Kernel", Root -> "Kernel", Context -> "ImportX`"}
+    }
+]
